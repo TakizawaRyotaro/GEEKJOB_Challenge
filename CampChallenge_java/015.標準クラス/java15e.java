368b77be8fee358e7f60aeeb03123d1523cbefae
滝澤 遼太郎 <rt0624rt0624rt0624@gmail.com>
2018-04-18 10:41:32 +0900

    java課題15-5[バイト数と文字数の取得]提出します。

---
+++ CampChallenge_java/015.標準クラス/java15e.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java15;
+
+/**
+ *
+ * @author takizawaryoutarou
+ */
+public class java15e {
+
+    public static void main(String[] args) {
+        String word = "滝澤遼太郎";
+//文字数を表示        
+        System.out.println(word.length());
+//バイト数を表示        
+        System.out.println(word.getBytes().length);
+
+    }
+
+}
