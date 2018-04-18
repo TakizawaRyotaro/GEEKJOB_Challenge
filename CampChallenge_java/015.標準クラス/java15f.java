5c3cde58a9d9d8997e28649a71d1bccf9d19f37f
滝澤 遼太郎 <rt0624rt0624rt0624@gmail.com>
2018-04-18 10:44:06 +0900

    java課題15-6[文字数の取得]提出します

---
+++ CampChallenge_java/015.標準クラス/java15f.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java15;
+
+/**
+ *
+ * @author takizawaryoutarou
+ */
+public class java15f {
+
+    public static void main(String[] args) {
+        String mail = "rt06rt24@docomo.ne.jp";
+//＠以降の文字数を表示する    
+        System.out.print(mail.substring(8).length());
+
+    }
+
+}
